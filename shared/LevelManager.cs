69c6250a85cc0681371eb2de507020ffc47067f1
Brago <bragolab@gmail.com>
2016-03-23 11:45:03 +0100

    Added files via upload

---
+++ shared/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour {
+
+	// Riferimenti controllers
+	public GameController GameControllerInstance;
+	// Settings del livello
+	public int Level;
+	public List<Transform> SpawnPointsForEnemies;
+	// Variabili di comportamento
+	public bool isMandatorySaveNPC;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void SetupScene(){
+		GameControllerInstance.Level = Level;
+		if(isMandatorySaveNPC){
+
+		}else{
+
+		}
+	}
+}
